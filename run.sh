1a948c2caa04a5f8163fb6bf718b639cf57b964c
singwai <singwai@Singwais-MacBook-Pro.local>
2018-06-04 09:27:10 -0400

    feat (brew): added more package, and group packages via array

--- run.sh
+++ run.sh
@@ -1,32 +1,60 @@
 #/bin/bash bash
-echo ****Installing xcode start****
-xcode-select --install
 
+################CONFIGUTATION START########################
+brew=(
+  awscli 
+  cheat
+  gnu-sed --with-default-names
+  openssl
+  python
+  vim --with-override-system-vi
+  wget --with-iri
+  cask
+)
+
+cask=(
+  1password
+  alfred
+  flux
+  google-chrome
+  google-backup-and-sync  
+  iterm2
+  java
+  nordvpn
+  qlcolorcode
+  qlimagesize
+  qlmarkdown
+  qlstephen
+  quicklook-json
+  slack
+  sublime-text
+  virtualbox
+  vlc
+)
+
+
+################ CONFIGUTATION END ########################
+
+echo Installing xcode? [Y/N]
+read -n 1 input
+if [$input == "y"]; then
+  echo ****Installing xcode start****
+  xcode-select --install
+else
+  echo "Skipping xcode-select"
+fi
 echo ****Installing brew start****
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo ****Updating brew start****
 brew update
-echo ****Install openssl****
-brew install openssl
-brew link openssl --force
-echo ****Updating install java google-chrome vlc video player****
-brew cask install java google-chrome vlc
-echo ****Install Sublime****
-brew cask install sublime-text
-echo ****Install Android Studio****
-brew cask install android-studio***
-echo ***Google Drive ****
-brew cask install google-drive
-echo *** Install Virtual Box ****
-brew cask install virtualbox;
-echo ** Install Flux ****
-brew cask install flux
-echo *** Install 1Password ****
-brew cask install 1password
-echo *** Install Slack ****
-brew cask install slack
 
+#echo **** install ${brew[@]} ****
+#brew **** install ${brew[@]} ****
+brew cask install ${cask[@]}
+#brew link openssl --force
+
+exit 1
 
 which = $(which pip)
 echo ${which}
@@ -40,9 +68,6 @@ echo ****install awscli start****
 sudo -H  pip install awscli --upgrade --ignore-installed six
 echo ****install awscli finish****
 
-echo ****ability to view unknown file format on mac previrew****
-echo ****See https://github.com/whomwah/qlstephen****
-brew cask install qlstephen
 #Support bigger file size. default is 100k
 defaults write com.whomwah.quicklookstephen maxFileSize 1024000
 
@@ -63,4 +88,8 @@ echo ****  Don\'t write DS_Store file on USB and network drive ****
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+echo "Cleanup"
+brew cleanup
+brew cask cleanup
 
+#https://apple.stackexchange.com/questions/306867/can-defaults-write-command-line-configure-the-menu-bar-on-macos
